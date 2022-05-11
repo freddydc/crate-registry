ded2a5e143dedc85f4866b1709caf9b90ade19a9
freddydc <freddydiaz.liv@gmail.com>
2022-05-11 11:17:42 -0500

    Add: Card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
+import { TodoCard } from './components/todo'
+
 export function App() {
-  return (
-    <div className="app">
-      <h1>Welcome!</h1>
-    </div>
-  )
+  return <TodoCard />
 }
---
+++ src/components/todo/TodoCard/TodoCard.tsx
@@ -0,0 +1,9 @@
+import styles from './TodoCard.module.css'
+
+export const TodoCard = () => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.title}>Crate registry</div>
+    </div>
+  )
+}
